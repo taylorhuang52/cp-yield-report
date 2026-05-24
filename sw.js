f5f63d24fbeb7dfff33d74efdfddd72086c2a76b
Taylor Huang <taylorhuang52@email.com>
2026-05-24 23:27:05 +0800

    correct discount/ reject rule

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cp-yield-report-v4';
+const CACHE_NAME = 'cp-yield-report-v5';
 const STATIC_ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
